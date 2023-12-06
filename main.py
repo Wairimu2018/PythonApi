97e4e26f36331ce7f8e57795a24c3a00f7f51c6b
Wairimu2018 <wairimukuria.wk@gmail.com>
2023-12-06 20:33:07 +0300

    render template

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/")
@@ -26,10 +26,10 @@ def get_user(user_id):
 
 @app.route("/create-user", methods=["POST"])
 def create_user():
-    # if request.method == "POST"
+   
     data = request.get_json()
 
     return jsonify(data), 201
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port= 8000, debug=True)
